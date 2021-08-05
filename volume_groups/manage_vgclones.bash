7f902e390b7135b9a101205c640cc6e794bade5b
Jon Kohler <jon@nutanix.com>
2021-08-04 23:06:36 -0400

    first cut

--- volume_groups/manage_vgclones.bash
+++ volume_groups/manage_vgclones.bash
@@ -1,52 +1,111 @@
 #!/usr/bin/bash
 
-## Setup
-# This script is designed to run on a Centos/Redhat VM
-# that is used to mount clones of production VMs. This procedure is
-# commonly used by a backup proxy or development system. 
-#
-# The VM needs to be run in the same AHV cluster as the volume 
-# group to be backed up.
-#
-# To use this script
-# 1. Allow the userid running this script paswordless ssh access 
-#    to the CVM (configure through Prism) and the VM hosting 
-#    the production VG.
-# 2. Add freeze and thaw commands for the produciton application .
-# 2. Customize configuration parameters (Search for TODO-1).
-# 3. Add bacakup command (Search for TODO-3).
-#
-# Questions: Contact scott.fadden@nutanix.com 
-#
-# NOTE: Add error checking
+#------------------------------------------------------------------------------
 
+# Copyright (c) 2021 Nutanix Inc. All rights reserved.
+#
+# Owner:   epic@nutanix.com
+# Authors: scott.fadden@nutanix.com
+#          jon@nutanix.com
 
-######
-# Conifugre these parameters to match your environment
-# TODO-1
-# Server to be backed up
-APP_IP="10.0.0.110"
-APP_ACCT="root"
+#------------------------------------------------------------------------------
 
-# Nutanix Details
-acct="nutanix"
-CVMIP="10.0.0.152"
-# Name of backup VM in AHV (not hostname)
-backupVM="Centos1"
+## About
+# This script is used to manage data cloning and out-of-system backups for
+# Epic's Operational Database, based on InterSystems IRIS, when running on
+# top of Nutanix AOS storage and Nutanix Acropolis Hypervisor (AHV).
+#
+# This script is intended to be a functional sample to assist with managing
+# backups in a production Epic ODB implementation. Modifications will be
+# required to "personalize" this script to the target environment.
+#
+# This script is designed to run within a CentOS/Redhat VM that does *not*
+# run Epic ODB itself, but rather acts as an "utility" VM to both schedule
+# the execution of this script as well as act as the "backup target" for
+# your chosen 3rd party backup software.
+#
+# This "utility" VM needs to be run in the same AHV cluster as the Epic ODB
+# instances to be backed up, so that we can mount the volume groups in a
+# hot-add manner.
+#
+# The script is designed to handle a single Epic environment at a time, such
+# that you can have multiple instantiations of this same script to manage the
+# various Epic environments (e.g. PRD, TST, RPT, DR).
 
-# Nutanix volume group to be cloned
-vg[0]="app-vg1"
+#------------------------------------------------------------------------------
+
+## General Script Logic
+# - Parse through existing VG (volume group) clones to see if we need to clean
+#   up any legacy clones that have aged out
+# - Freeze target IRIS instance (e.g. PRD, TST, RPT, DR)
+# - Take a VG clone on Nutanix AOS
+# - Thaw target IRIS instance
+# - Mount VG clone to a "mount" VM
+# - Kick off backup job in 3rd party out-of-system backup provider
+#   - e.g. Veeam
+
+#------------------------------------------------------------------------------
+
+## Script Usage Instructions
+# 1. Allow the userid running this script passwordless ssh access
+#    to the CVM (configure through Prism) and the VM hosting
+#    the target VG.
+# 2. Add freeze and thaw commands for the target Epic environment.
+# 3. Customize Environment Configuration Variables.
+# 4. Add 3rd party backup command(s).
+
+#------------------------------------------------------------------------------
+
+## Environment Configuration Variables
+# Configure these parameters to match the target environment
+
+# Target ODB VM Details
+# - This would be a VM on the same cluster as the AHV_MOUNT_VM
+# - This is used to reach into that VM and do the freeze/thaw commands
+TARGET_ODB_IP="10.0.0.110"
+TARGET_ODB_ACCT="root"
+TARGET_ODB_ENV[0]="poc"
+
+# Nutanix AOS Cluster Details
+# - Use the IP of any AOS CVM (controller VM) in the target cluster
+CVM_ACCT="nutanix"
+CVM_IP="10.0.0.152"
+
+# Utility/Mount VM Details
+# - This is the VM in AHV that will mount the disk clones
+AHV_MOUNT_VM="Centos1"
+
+# Nutanix Volume Group (VG) name
+# - This script assumes there is a single data volume group per backup
+# - This VG may contain many individual vdisks and this script assumes that all
+#   disk(s) within the Nutanix-side volume group are aggregated into
+#   a Linux LVM2 volume group.
+NTNX_VG[0]="app-vg1"
+
 # Linux LVM Volume Group name
-lvmvg[0]="prdvg"
+LVM_VG[0]="prdvg"
+
+# TEMPORARY FOR BH TST
+VMDISK_UUID[0]="58e2606a-055c-40da-9f71-58cf55957936"
 
 # Mount point for volume group
-mp[0]="/backup/prd01"
+MP[0]="/backup/prd01"
 
-# Number of clones to keep  
-num_keep=4
+# Number of clones to keep
+# - This is useful to keep a couple of the recent clones on the system, such
+#   that if rapid recovery is needed, the snapshot/clone data is already
+#   on-disk.
+# - The script assumes that backups will be run nightly and that keeping
+#   excessive copies is unproductive, given that the delta change rate day
+#   over day makes older data less and less useful.
+NUM_KEEP=2
 
-# End Customizations
-####################
+#------------------------------------------------------------------------------
+
+## Script Helpers
+
+PREFIX_DATE=`date +%s`
+ACLI="/usr/local/nutanix/bin/acli"
 
 # Function:  getmyvmid
 #   Match this hostname to the vm_uuid
@@ -59,85 +118,141 @@ getmyvmid () {
 #   Match this hostname to the vm name
 getmyvmname () {
   ### What host am I on?
-  vmname=`ssh ${acct}@${CVMIP} "/usr/local/nutanix/bin/acli vm.list | grep ${1} " 2> /dev/null | awk '{ print $1}'`
+  vmname=`ssh ${CVM_ACCT}@${CVM_IP} "${ACLI} vm.list | grep ${1} " 2> /dev/null | awk '{ print $1}'`
   echo $vmname
 }
 myvmid=$(getmyvmid)
 myvmname=$(getmyvmname $myvmid)
 echo "This VM name is " $myvmname " vm_uuid "  $myvmid
 
-## Step 1: Check for old clones and remove any over num_keep
+#------------------------------------------------------------------------------
+
+## Step 1: Check for old clones and remove any over NUM_KEEP
 # Clone order is based on EPOCH timestamp name prefix
 
-# Unmount cloned file system
-umount ${mp[0]}
+# TEMPORARY - COMMENTED OUT FOR BH TST
+# # Unmount cloned file system
+# umount ${MP[0]}
 
-# Detach existing clones from VM
-echo "Detach previous clone"
-for i in `ssh ${acct}@${CVMIP} "/usr/local/nutanix/bin/acli vg.list | grep 'copy-${vg[0]}' | awk '{ print $1 }'" 2> /dev/null`
-do
-  cnt=`ssh ${acct}@${CVMIP} "/usr/local/nutanix/bin/acli vg.get ${i} | grep 'vm_uuid:.*${myvmid}' | wc -l " 2> /dev/null`
-  #echo "Count for " $i " is " $cnt
-  if (( cnt > 0 )); then
-      ret=`ssh ${acct}@${CVMIP} "/usr/local/nutanix/bin/acli vg.detach_from_vm ${i} ${myvmname} " 2> /dev/null`
-      echo "Detached clone " ${i} " Ret = " $ret
-  fi
-done
+# # Detach existing clones from VM
+# echo "Detach previous clone"
+# for i in `ssh ${CVM_ACCT}@${CVM_IP} "${ACLI} vg.list | grep 'copy-${NTNX_VG[0]}' | awk '{ print $1 }'" 2> /dev/null`
+# do
+#   cnt=`ssh ${CVM_ACCT}@${CVM_IP} "${ACLI} vg.get ${i} | grep 'vm_uuid:.*${myvmid}' | wc -l " 2> /dev/null`
+#   #echo "Count for " $i " is " $cnt
+#   if (( cnt > 0 )); then
+#       ret=`ssh ${CVM_ACCT}@${CVM_IP} "${ACLI} vg.detach_from_vm ${i} ${myvmname} " 2> /dev/null`
+#       echo "Detached clone " ${i} " Ret = " $ret
+#   fi
+# done
 
-numclone=`ssh ${acct}@${CVMIP} "/usr/local/nutanix/bin/acli vg.list | grep [0-9].*-copy-${vg[0]} | wc -l" 2> /dev/null`
+# numclone=`ssh ${CVM_ACCT}@${CVM_IP} "${ACLI} vg.list | grep [0-9].*-copy-${NTNX_VG[0]} | wc -l" 2> /dev/null`
 
-# Delete expired clones
-echo "Current Number of Clones " $numclone " for " ${vg[0]}
-while(( numclone >= num_keep )); do
-  rmvg=`ssh ${acct}@${CVMIP} "/usr/local/nutanix/bin/acli vg.list | /usr/bin/grep [0-9].*-copy-${vg[0]} | /usr/bin/sort -n | /usr/bin/head -1 | /usr/bin/sed 's/  /\:/'"  2> /dev/null`
-  echo "Removing VG " ${rmvg}
-  echo  ${acct}@${CVMIP} "/usr/bin/echo yes | /usr/local/nutanix/bin/acli vg.delete ${rmvg}"
-  ssh ${acct}@${CVMIP} "/usr/bin/echo yes | /usr/local/nutanix/bin/acli vg.delete ${rmvg}"
-  numclone=`ssh ${acct}@${CVMIP} /usr/local/nutanix/bin/acli vg.list | grep [0-9].*-copy-${vg[0]} | wc -l`
-done
+# # Delete expired clones
+# echo "Current Number of Clones " $numclone " for " ${NTNX_VG[0]}
+# while(( numclone >= NUM_KEEP )); do
+#   rmvg=`ssh ${CVM_ACCT}@${CVM_IP} "${ACLI} vg.list | /usr/bin/grep [0-9].*-copy-${NTNX_VG[0]} | /usr/bin/sort -n | /usr/bin/head -1 | /usr/bin/sed 's/  /\:/'"  2> /dev/null`
+#   echo "Removing VG " ${rmvg}
+#   echo  ${CVM_ACCT}@${CVM_IP} "/usr/bin/echo yes | ${ACLI} vg.delete ${rmvg}"
+#   ssh ${CVM_ACCT}@${CVM_IP} "/usr/bin/echo yes | ${ACLI} vg.delete ${rmvg}"
+#   numclone=`ssh ${CVM_ACCT}@${CVM_IP} ${ACLI} vg.list | grep [0-9].*-copy-${NTNX_VG[0]} | wc -l`
+# done
+
+#------------------------------------------------------------------------------
+
+## TEMP FOR BH TST
+# Create a tmp volumegroup. This will be used to hold the vmdisk clones below
+# since the BH TST environment was configured with vdisks and not VGs
+ssh ${CVM_ACCT}@${CVM_IP} "${ACLI} vg.create ${PREFIX_DATE}-copy-${TARGET_ODB_ENV[0]}"
+
+#------------------------------------------------------------------------------
 
 ## Step 2: Freeze Application
 # Replace the following line with application freeze command
-# TODO-2
-ssh ${APP_ACCT}@${APP_IP} "echo -n 'Pause Application on Server: ' ; /bin/hostname "
+# TODO: put the actual environment freeze commands here
+ssh ${TARGET_ODB_ACCT}@${TARGET_ODB_IP} "echo -n 'Pause Application on Server: ' ; /bin/hostname "
 echo ""
 
+#------------------------------------------------------------------------------
+
 ## Step 3: Clone the VG
-pre=`date +%s`
-echo "Creating new clone " ${pre}-copy-${vg[0]}
-ssh ${acct}@${CVMIP} /usr/local/nutanix/bin/acli vg.clone ${pre}-copy-${vg[0]} clone_from_vg=${vg[0]}
-   
+# TEMPORARY COMMENTING OUT FOR BH TST
+# echo "Creating new clone " ${PREFIX_DATE}-copy-${NTNX_VG[0]}
+# ssh ${CVM_ACCT}@${CVM_IP} ${ACLI} vg.clone ${PREFIX_DATE}-copy-${NTNX_VG[0]} clone_from_vg=${NTNX_VG[0]}
+
+# TEMP FOR BH TST
+# - We'll clone the vdisk attached to the BH TST environment into a VG, which
+#   allows us to treat the rest of the logic in this script just like we would
+#   if the original setup was with a VG. This configuration will be harmonized
+#   for this environment at a future date, such that TST will match production
+#   which will be exclusively VGs
+ssh ${CVM_ACCT}@${CVM_IP} "${ACLI} vg.disk_create \
+  ${PREFIX_DATE}-copy-${TARGET_ODB_ENV[0]} \
+  clone_from_vmdisk=${VMDISK_UUID[0]}"
+
+#------------------------------------------------------------------------------
 
 ## Step 4: Thaw Application
 # Replace the following line with application thaw command
-# TODO-2
-ssh ${APP_ACCT}@${APP_IP} "echo -n "Resume the Application on Server: " ; /bin/hostname "
+# TODO: put the actual environment thaw commands here
+ssh ${TARGET_ODB_ACCT}@${TARGET_ODB_IP} "echo -n "Resume the Application on Server: " ; /bin/hostname "
 echo ""
 
-## Step 5: Mount the clone
+#------------------------------------------------------------------------------
 
+## Step 5: Mount the clone
 # Attach new clone
-echo "Attach " ${pre}-copy-${vg[0]}
-ssh ${acct}@${CVMIP} "/usr/local/nutanix/bin/acli vg.attach_to_vm ${pre}-copy-${vg[0]} ${backupVM}"
+echo "Attach " ${PREFIX_DATE}-copy-${NTNX_VG[0]}
+ssh ${CVM_ACCT}@${CVM_IP} "${ACLI} vg.attach_to_vm ${PREFIX_DATE}-copy-${NTNX_VG[0]} ${AHV_MOUNT_VM}"
 
 # Clean up LVM metadata
 pvscan --cache
 
 # Get device name
-dev_path=`/usr/sbin/lvdisplay ${lvmvg[0]} | awk '{ if( $2 == "Path" ) print $3 }'`
+dev_path=`/usr/sbin/lvdisplay ${LVM_VG[0]} | awk '{ if( $2 == "Path" ) print $3 }'`
 
 # Mount the File System
-/usr/bin/mount $dev_path ${mp[0]}
+/usr/bin/mount $dev_path ${MP[0]}
 df
-ret=`/usr/bin/df | grep "${mp[0]}" | wc -l`
+ret=`/usr/bin/df | grep "${MP[0]}" | wc -l`
 if(( ret == 1 )); then
-  echo "Backup file system ${mp[0]} is ready."
+  echo "Backup file system ${MP[0]} is ready."
 else
-  echo "Backup file system ${mp[0]} did not mount properly"
+  echo "Backup file system ${MP[0]} did not mount properly"
 fi
 
-## Step 6: Kick off backup
-# TODO-3
-# Add backup command here
-echo "Add vendor specific backup command here"
+#------------------------------------------------------------------------------
 
+## Step 6: Kick off 3rd party out-of-system backup
+echo "Kick off backup for " ${PREFIX_DATE}-copy-${NTNX_VG[0]}
+
+# Endpoint URL for login action
+veeamUsername="EMUsername" #Your username, if using domain based account, enter UPN (user@domain.com)
+veeamPassword="EMPassword"
+veeamAuth=$(echo -ne "$veeamUsername:$veeamPassword" | base64);
+veeamRestServer="EMServer" #IP Address or FQDN of Enterprise Manager server
+veeamRestPort="9398" #Default HTTPS REST API Port
+veeamSessionId=$(curl -X POST "https://$veeamRestServer:$veeamRestPort/api/sessionMngr/?v=latest" -H "Authorization:Basic $veeamAuth" -H "Content-Length: 0" -H "Accept: application/json" -k --silent | awk 'NR==1{sub(/^\xef\xbb\xbf/,"")}1' | jq --raw-output ".SessionId")
+veeamXRestSvcSessionId=$(echo -ne "$veeamSessionId" | base64);
+veeamJobId="763797f3-391c-46c8-aa81-83d04f534396"
+
+# Query Job
+veeamEMJobUrl="https://$veeamRestServer:$veeamRestPort/api/jobs/$veeamJobId?format=Entity"
+veeamEMJobDetailUrl=$(curl -X GET "$veeamEMJobUrl" -H "Accept:application/json" -H "X-RestSvcSessionId: $veeamXRestSvcSessionId" -H "Cookie: X-RestSvcSessionId=$veeamXRestSvcSessionId" -H "Content-Length: 0" --insecure 2>&1 -k --silent | awk 'NR==1{sub(/^\xef\xbb\xbf/,"")}1')
+
+# Start Job
+veeamEMStartUrl="https://$veeamRestServer:$veeamRestPort/api/jobs/$veeamJobId?action=start"
+veeamEMResultUrl=$(curl -X POST "$veeamEMStartUrl" -H "Accept:application/json" -H "X-RestSvcSessionId: $veeamXRestSvcSessionId" -H "Cookie: X-RestSvcSessionId=$veeamXRestSvcSessionId" -H "Content-Length: 0" --insecure 2>&1 -k --silent | awk 'NR==1{sub(/^\xef\xbb\xbf/,"")}1')
+
+# Capture & Display Results
+veeamJobName=$(echo "$veeamEMJobDetailUrl" | jq --raw-output ".Name")
+veeamTaskId=$(echo "$veeamEMResultUrl" | jq --raw-output ".TaskId")
+veeamState=$(echo "$veeamEMResultUrl" | jq --raw-output ".State")
+veeamOperation=$(echo "$veeamEMResultUrl" | jq --raw-output ".Operation")
+
+#------------------------------------------------------------------------------
+
+# TODOs
+# - squash Scott's reg_with_cvm and reg_with_uvm functions to automatically
+#   handle getting passwordless SSH setup.
+# - add error handling
